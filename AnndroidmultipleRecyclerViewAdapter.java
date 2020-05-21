da4facfde6ad84222214164518e9987f44804614
Ritesh Singh <35375886+RiteshF7@users.noreply.github.com>
2020-05-21 15:08:23 +0530

    Update AnndroidmultipleRecyclerViewAdapter.java

--- AnndroidmultipleRecyclerViewAdapter.java
+++ AnndroidmultipleRecyclerViewAdapter.java
@@ -1,155 +1,120 @@
 package tutorialslink.com.tutorialslinkwebview.adapters;
 
 import android.content.Context;
-import android.media.MediaPlayer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.cardview.widget.CardView;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import tutorialslink.com.tutorialslinkwebview.R;
-import tutorialslink.com.tutorialslinkwebview.pojos.ModelHomeMRV;
-
-public class HomeFragMultipleViewRvAdapter extends RecyclerView.Adapter {
-
-    List<ModelHomeMRV> dataSet = new ArrayList<>();
-    Context mContext;
-
-    public static class TextTypeViewHolder extends RecyclerView.ViewHolder {
-
-        TextView txtType;
+import tutorialslink.com.tutorialslinkwebview.pojos.editprofilemodles.EditInfoHeader;
+import tutorialslink.com.tutorialslinkwebview.pojos.editprofilemodles.EditInfoModel;
+import tutorialslink.com.tutorialslinkwebview.pojos.editprofilemodles.EditPhotoModel;
+import tutorialslink.com.tutorialslinkwebview.pojos.editprofilemodles.EditProfileModel;
 
 
-        public TextTypeViewHolder(View itemView) {
-            super(itemView);
-            this.txtType = (TextView) itemView.findViewById(R.id.type);
-        }
-    }
+public class AdapterEditProfileMultipleRV extends RecyclerView.Adapter{
 
-    public static class ImageTypeViewHolder extends RecyclerView.ViewHolder {
+    static final int EDIT_PHOTO_VIEW = R.layout.rv_layout_edit_photo;
+    static final int EDIT_INFO_VIEW = R.layout.rv_layout_edit_info;
+    static final int INFO_HEADER_VIEW = R.layout.rv_layout_edit_info_header;
 
-
-
-
-        public ImageTypeViewHolder(View itemView) {
-            super(itemView);
-
-        }
-    }
-
-    public static class AudioTypeViewHolder extends RecyclerView.ViewHolder {
-
-
-        public AudioTypeViewHolder(View itemView) {
-            super(itemView);
-
-
-        }
-    }
-
-    public HomeFragMultipleViewRvAdapter( Context context,List<ModelHomeMRV> dataSet) {
-        this.dataSet=dataSet;
-        this.mContext = context;
+    List<EditProfileModel> dataSet = new ArrayList<>();
+    Context context;
+    LayoutInflater layoutInflater;
+    public AdapterEditProfileMultipleRV(List<EditProfileModel> dataSet, Context context) {
+        this.dataSet = dataSet;
+        this.context = context;
+        layoutInflater =LayoutInflater.from(context);
 
     }
 
+    @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
-        switch (viewType) {
-            case ModelHomeMRV.TEXT_TYPE:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.text_type, parent, false);
-                return new TextTypeViewHolder(view);
-            case ModelHomeMRV.IMAGE_TYPE:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_type, parent, false);
-                return new ImageTypeViewHolder(view);
-            case ModelHomeMRV.AUDIO_TYPE:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.audio_type, parent, false);
-                return new AudioTypeViewHolder(view);
-        }
-        return null;
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-
-        switch (dataSet.get(position).type) {
-            case 0:
-                return ModelHomeMRV.TEXT_TYPE;
-            case 1:
-                return ModelHomeMRV.IMAGE_TYPE;
-            case 2:
-                return ModelHomeMRV.AUDIO_TYPE;
+        switch (viewType){
+            case EDIT_PHOTO_VIEW:
+                 view =layoutInflater.inflate(EDIT_PHOTO_VIEW,parent,false);
+                return new EditPhotoViewHolder(view);
+            case EDIT_INFO_VIEW:
+                 view =layoutInflater.inflate(EDIT_INFO_VIEW,parent,false);
+                return new EditPhotoViewHolder(view);
+            case INFO_HEADER_VIEW:
+                 view =layoutInflater.inflate(INFO_HEADER_VIEW,parent,false);
+                return new EditPhotoViewHolder(view);
             default:
-                return -1;
+                view = layoutInflater.inflate(R.layout.default_rv_view_null,parent,false);
+                return new DefaultView(view);
         }
+
     }
 
     @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int listPosition) {
-
-        ModelHomeMRV object = dataSet.get(listPosition);
-        if (object != null) {
-            switch (object.type) {
-                case ModelHomeMRV.TEXT_TYPE:
-                    ((TextTypeViewHolder) holder).txtType.setText(object.text);
-
-                    break;
-                case ModelHomeMRV.IMAGE_TYPE:
-
-                    break;
-                case ModelHomeMRV.AUDIO_TYPE:
-
-                    break;
-            }
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Object currentObject = dataSet.get(position);
+        if(holder instanceof EditPhotoViewHolder){
+            EditPhotoModel editPhotoModel = (EditPhotoModel) currentObject;
         }
+        else if (holder instanceof EditInfoViewHolder){
+            EditInfoModel editPhotoModel = (EditInfoModel) currentObject;
+        }
+        else if (holder instanceof InfoHeaderViewHolder){
+            EditInfoHeader editPhotoModel = (EditInfoHeader) currentObject;
+        }
+
     }
 
     @Override
     public int getItemCount() {
         return dataSet.size();
     }
-}
 
-
-package tutorialslink.com.tutorialslinkwebview.pojos;
-
-public class ModelHomeMRV {
-
-    public static final int TEXT_TYPE=0;
-    public static final int IMAGE_TYPE=1;
-    public static final int AUDIO_TYPE=2;
-
-    public int type;
-    public int data;
-    public String text;
-
-    public ModelHomeMRV(int type, String text, int data)
-    {
-        this.type=type;
-        this.data=data;
-        this.text=text;
+    @Override
+    public int getItemViewType(int position) {
+        switch (dataSet.get(position).getType()){
+            case 0:
+                return EDIT_PHOTO_VIEW;
+            case 1:
+                return INFO_HEADER_VIEW;
+            case 2:
+                return  EDIT_INFO_VIEW;
+            default:
+                return -1;
+        }
     }
+
+
+
+    public class DefaultView extends RecyclerView.ViewHolder{
+
+        public DefaultView(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    public class EditPhotoViewHolder extends RecyclerView.ViewHolder{
+
+        public EditPhotoViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    public class EditInfoViewHolder extends RecyclerView.ViewHolder{
+
+        public EditInfoViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    public class InfoHeaderViewHolder extends RecyclerView.ViewHolder{
+
+        public InfoHeaderViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
-
-//implementation
-
- ArrayList<Model> list= new ArrayList();
-        list.add(new Model(Model.TEXT_TYPE,"Hello. This is the Text-only View Type. Nice to meet you",0));
-        list.add(new Model(Model.IMAGE_TYPE,"Hi. I display a cool image too besides the omnipresent TextView.",R.drawable.wtc));
-        list.add(new Model(Model.AUDIO_TYPE,"Hey. Pressing the FAB button will playback an audio file on loop.",R.raw.sound));
-        list.add(new Model(Model.IMAGE_TYPE,"Hi again. Another cool image here. Which one is better?",R.drawable.snow));
-
-        MultiViewTypeAdapter adapter = new MultiViewTypeAdapter(list,this);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, OrientationHelper.VERTICAL, false);
-
